c717457f2fcc463a37b3b1d546606389c4de0d99
AliCW <aliwhitrow1@gmail.com>
2022-12-13 11:02:33 +0000

    3-GET-api-topics completed, SQL JOIN query tested

--- model/model.js
+++ model/model.js
@@ -1,11 +1,33 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
 const findAllTopics = () => {
-    return db.query(`
-    SELECT * FROM topics;`)
-    .then(({rows}) => {
-        return rows;
-    })
-}
+  return db
+    .query(
+      `
+    SELECT * FROM topics;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
 
-module.exports = { findAllTopics }
+const findArticles = () => {
+  return db
+    .query(
+      `
+    SELECT 
+    articles.author, articles.title, articles.article_id, 
+    articles.topic, articles.created_at, articles.votes, COUNT(comments.article_id) AS comments_count
+    FROM articles
+    FULL OUTER JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.title, articles.article_id, 
+    articles.topic, articles.created_at, articles.votes
+    ORDER BY articles.created_at DESC
+    `
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
+module.exports = { findAllTopics, findArticles };
